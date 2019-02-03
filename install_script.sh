5599ce699b77cb7f0be6f9ea0d7c87eba4a51147
Andre Natal <anatal@gmail.com>
2019-02-02 22:42:26 -0800

    fixing permission

--- install_script.sh
+++ install_script.sh
@@ -40,6 +40,7 @@ install_pkg() {
 }
 
 sudo unzip -o assistant_proj.zip -d /usr/share/snips
+sudo chown -R _snips:_snips /usr/share/snips/assistant/
 cp -r personal_kws_tpl personal_kws
 sudo rm -rf /etc/snips.toml
 install_pkg "snips-injection"
